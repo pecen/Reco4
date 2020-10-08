b4ddfcd2b8dc2e6f8e4fb50fa2e9b2a935d15124
503212497 <peter.centellini@cytiva.com>
2020-10-08 18:26:05 +0200

    Removed unnecessary usings. Added Nuget package. Removed EF package from Dal-project.

--- src/Reco4.Dal/Extensions/TimeStampExtensions.cs
+++ src/Reco4.Dal/Extensions/TimeStampExtensions.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Reco4.Dal.Extensions {
   public static class TimeStampExtensions {
